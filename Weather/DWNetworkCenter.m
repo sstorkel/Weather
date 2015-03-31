af68bab44a5f10b50f0bf53ee0b6b89e1f1456f7
Scott Storkel <sstorkel@devious.com>
2015-03-31 14:56:36 -0700

    Implement simple caching

--- Weather/DWNetworkCenter.m
+++ Weather/DWNetworkCenter.m
@@ -14,10 +14,25 @@
 //static const NSString* APPID = @"58abcad7dc09bfcd9d42b1f7a0e02e96";
 static const NSString* APPID = @"";
 
+static const NSUInteger CACHE_SIZE_LIMIT = 10;  // cache weather for 10 locations at most
+
+
+@interface DWCacheObject : NSObject
+@property (nonatomic, strong) id       response;
+@property (nonatomic, copy)   NSDate*  downloadTime;
+@end
+
+@implementation DWCacheObject
+@end
+
+
 
 @interface DWNetworkCenter ()
 {
     AFHTTPRequestOperation* currentRequest;
+    
+    // Cache containing responses to API calls
+    NSCache* cache;
 }
 @end
 
@@ -36,6 +51,16 @@ static const NSString* APPID = @"";
     return theInstance;
 }
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        cache = [NSCache new];
+        cache.countLimit = CACHE_SIZE_LIMIT;
+    }
+    return self;
+}
+
 - (void)getWeatherFromURL:(NSString*)url completion:(DWNetworkCompletionBlock)completionBlock
 {
 #ifdef DEBUG
@@ -51,10 +76,32 @@ static const NSString* APPID = @"";
         [currentRequest cancel];
     }
     
+    // Check to see if URL has been cached:
+    DWCacheObject* obj = [cache objectForKey:url];
+    if (obj) {
+        if ([obj.downloadTime timeIntervalSinceNow] > -10 * 60) {
+#ifdef DEBUG
+            NSLog(@"Cache hit!");
+#endif
+            // cached response is still valid; pass it to the completion block
+            // and exit
+            completionBlock(obj.response, nil);
+            return;
+            
+        }
+    }
+    
     currentRequest = [manager GET:url
                        parameters:nil
                           success:^(AFHTTPRequestOperation *operation, id responseObject) {
                               self->currentRequest = nil;
+                              
+                              // Cache response
+                              DWCacheObject* co = [DWCacheObject new];
+                              co.response = responseObject;
+                              co.downloadTime = [NSDate new];
+                              [self->cache setObject:co forKey:url];
+                              
                               completionBlock(responseObject, nil);
                           }
                           failure:^(AFHTTPRequestOperation *operation, NSError *error) {
@@ -63,9 +110,49 @@ static const NSString* APPID = @"";
                           }];
 }
 
+// Normalize city strings in an effort to improve cache efficiency. Ideally,
+// we want "Mountain View,US" and "  mountain view , us  " to hash to the same
+// cache entry. We separate the string into components using ',' as the divider,
+// trim whitespace from the beginning and ends of the components, lowercase the
+// strings, then reassemble.
+//
+// NOTE: we don't currently collapse spaces that appear between words in a single
+// component. So, for example, "Mountain View" and "Mountain    View" are normalized
+// to two different strings
+//
+- (NSString*)normalizeString:(NSString*)str
+{
+    NSArray* components = [str componentsSeparatedByString:@","];
+    if (components.count == 0) {
+        return str.lowercaseString;
+    } else if (components.count == 1) {
+        NSString* s = components[0];
+        s = [s stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        return s.lowercaseString;
+    } else {
+        NSMutableString* result = [NSMutableString new];
+        
+        for (int i=0; i < components.count-1; ++i) {
+            NSString* s = components[i];
+            s = [s stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            [result appendString:s.lowercaseString];
+            if (s.length > 0)
+                [result appendString:@","];
+        }
+        
+        NSString* s = components[components.count-1];
+        s = [s stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+        [result appendString:s.lowercaseString];
+        
+        return result;
+    }
+}
+
 - (void)getWeatherForCity:(NSString*)city completion:(DWNetworkCompletionBlock)completionBlock
 {
-    NSString* encodedCity = [city stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString* normalizedCity = [self normalizeString:city];
+    NSString* encodedCity = [normalizedCity stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
     NSString* url = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?q=%@&APPID=%@&units=imperial", encodedCity, APPID];
     [self getWeatherFromURL:url completion:completionBlock];
 }
@@ -75,5 +162,6 @@ static const NSString* APPID = @"";
     NSString* url = [NSString stringWithFormat:@"http://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&APPID=%@&units=imperial", location.coordinate.latitude, location.coordinate.longitude, APPID];
     [self getWeatherFromURL:url completion:completionBlock];
 }
+
 
 @end
